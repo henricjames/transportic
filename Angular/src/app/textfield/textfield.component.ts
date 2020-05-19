66868be97596c1bd913be0b23826fe0541cfe825
amrithas <amrithas@opentrends.net>
2020-05-19 11:32:38 +0530

    initial commit

---
+++ Angular/src/app/textfield/textfield.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-textfield',
+  templateUrl: './textfield.component.html',
+  styleUrls: ['./textfield.component.scss']
+})
+export class TextfieldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
