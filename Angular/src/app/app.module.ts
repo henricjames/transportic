53d415467451bdab19df33cce981e361ce86b659
amrithas <amrithas@opentrends.net>
2020-05-26 14:06:20 +0530

    changed the html structure and added the textfield as childcomponent

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,22 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DummyComponentComponent } from './shared-components/dummy-component/dummy-component.component';
-import { TextfieldComponent } from './textfield/textfield.component';
+import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TextfieldComponent } from './textfield/textfield.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    DummyComponentComponent,
-    TextfieldComponent
+    TextfieldComponent,
+    
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Angular/src/app/textfield/textfield.component.ts
+++ Angular/src/app/textfield/textfield.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input} from '@angular/core';
 
 @Component({
   selector: 'app-textfield',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./textfield.component.scss']
 })
 export class TextfieldComponent implements OnInit {
+  @Input() labelname: string;
 
   constructor() { }
 
