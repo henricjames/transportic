4bd3f97f4b14aac530cd112078dc98a20c3c0f15
Henric <henricj@opentrends.net>
2020-06-11 09:23:07 +0530

    changed to new architecture

--- Architecture/src/app/shared/textfield/textfield.component.ts
+++ Architecture/src/app/shared/textfield/textfield.component.ts
@@ -1,29 +1,36 @@
-import { Component, OnInit,Input,ChangeDetectionStrategy} from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ChangeDetectionStrategy,
+} from '@angular/core';
 import { MakeProvider, AbstractValueAccessor } from './abstractclass';
-
 
 @Component({
   selector: 'app-textfield',
   templateUrl: './textfield.component.html',
   styleUrls: ['./textfield.component.scss'],
-  providers: [
-    MakeProvider(TextfieldComponent)
-  ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  providers: [MakeProvider(TextfieldComponent)],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TextfieldComponent extends AbstractValueAccessor implements OnInit {
-  
+export class TextfieldComponent extends AbstractValueAccessor
+  implements OnInit {
   @Input() labelname: string;
-  @Input() type: string="text";
+  @Input() type: 'text' | 'password' | 'email' = 'text';
+  @Input() showPasswordIcon: boolean;
 
-  constructor() { 
+  constructor() {
     super();
   }
 
-  ngOnInit(): void {
-  }
-  onInput(inputValue){
+  ngOnInit(): void {}
+  onInput(inputValue) {
     this.writeValue(inputValue);
   }
 
+  toggleVisibility() {
+    console.log(this.type);
+    if (this.type == 'password') this.type = 'text';
+    else this.type = 'password';
+  }
 }
