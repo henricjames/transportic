a19b0d61eb7c1544842d608f05ae95ad38e5be0b
Ismail <ithacher@opentrends.net>
2020-06-12 16:50:20 +0530

    lazy loading used

--- Architecture/src/app/auth/pages/registration/registration.component.ts
+++ Architecture/src/app/auth/pages/registration/registration.component.ts
@@ -1,5 +1,7 @@
+
+
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -7,10 +9,23 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
-  myForm: FormGroup;
-  constructor() { }
+  
+  transporticForm: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    console.log('it worked')
+    this.transporticForm = this.fb.group({
+      name: '',
+      //name: ['', Validators.required],
+      email: '',
+      message: ''
+    })
+    this.transporticForm.valueChanges.subscribe(console.log);
+  }
+  onSubmit(form: FormGroup) {
+    console.log('Valid?', form.valid);
+    console.log('Name', form.value.name);
   }
 
 }
