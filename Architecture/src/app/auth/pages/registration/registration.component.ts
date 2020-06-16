b17bbadff9b9cbb000545079e857416162e58ac9
Ismail <ithacher@opentrends.net>
2020-06-16 16:01:46 +0530

    feature stepper added

--- Architecture/src/app/auth/pages/registration/registration.component.ts
+++ Architecture/src/app/auth/pages/registration/registration.component.ts
@@ -8,17 +8,17 @@ import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.scss']
 })
-export class RegistrationComponent implements OnInit {
-  
+export class RegistrationComponent implements OnInit {  
   transporticFormInfo: FormGroup
   transporticFormAccount: FormGroup
   transporticFormEmail: FormGroup
-
   openedBool: boolean = false
   isActiveInfo: boolean = true
   isActiveAccount: boolean = false
-  isActiveEmail: boolean = false
-  isActive: boolean = false
+  isActiveEmail: boolean = false;
+  public infoStepper: boolean = true;
+  public accountStepper: boolean= false;
+  public emailStepper: boolean= false;
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
@@ -33,31 +33,44 @@ export class RegistrationComponent implements OnInit {
     this.transporticFormAccount = this.fb.group({
       email: '',
       password:''
+    }),
+    this.transporticFormEmail = this.fb.group({
+      email: ''
     })
     this.transporticFormInfo.valueChanges.subscribe(console.log);
   }
   formValidate(){
     alert('closed')
   }
-  onSubmit(form: FormGroup) {
-    this.openedBool = !this.openedBool    
-    this.isActiveInfo = !this.isActiveInfo;
-
+  onSubmit(form: FormGroup) { 
     // console.log(this.openedBool, 'Name', this.transporticFormInfo.value);
   }
   clickInfo(){
-    this.openedBool = !this.openedBool    
-    this.isActiveInfo = !this.isActiveInfo;
-    this.isActiveAccount = !this.isActiveAccount;
-    // this.transporticFormInfo.reset()
+      this.openedBool = !this.openedBool    
+      this.isActiveInfo = !this.isActiveInfo;
+      this.isActiveAccount = !this.isActiveAccount
+      this.transporticFormInfo.reset(this.transporticFormInfo.value)
+      console.log('Info Worked')
+      this.infoStepper= !this.infoStepper;       
+      this.accountStepper = true    
+      this.isActiveEmail = !this.isActiveAccount
   }
-  clickEmail(){
-    this.isActiveAccount = !this.isActiveAccount;
-    this.isActive = !this.isActive;    
+  clickEmail(){    
+    this.openedBool = !this.openedBool    
+      // this.isActiveInfo = !this.isActiveInfo;
+    this.isActiveAccount = !this.isActiveAccount
+    this.isActiveEmail = !this.isActiveEmail
+    this.emailStepper= !this.emailStepper;
+    console.log('Email Worked')
+    this.accountStepper=!this.accountStepper       
   }
   clickComplete(){
+    // this.openedBool = !this.openedBool    
+      // this.isActiveAccount = !this.isActiveAccount
     console.log('Completed')
-    this.isActive = !this.isActive;        
-    this.isActiveAccount = !this.isActiveAccount;    
+    // this.transporticFormEmail.reset(this.transporticFormEmail.value)
+    // this.isActiveEmail = !this.isActiveEmail;
+    //   this.isActiveInfo = !this.isActiveInfo;
+    // this.infoStepper = !this.infoStepper;
   }
 }
