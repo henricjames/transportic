f18e5fbacf1e71e9d0b724c644dbefd5125f8d9d
Henric <henricj@opentrends.net>
2020-07-01 09:57:56 +0530

    added translation,variables in styles

--- Architecture/src/app/auth/pages/forgotpassword/forgotpassword.component.ts
+++ Architecture/src/app/auth/pages/forgotpassword/forgotpassword.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-forgotpassword',
   templateUrl: './forgotpassword.component.html',
-  styleUrls: ['./forgotpassword.component.scss']
+  styleUrls: ['./forgotpassword.component.scss'],
 })
 export class ForgotpasswordComponent implements OnInit {
-
-  constructor() { }
+  forgotpasswordform: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.forgotpasswordform = this.formBuilder.group({
+      email: new FormControl('', [Validators.required]),
+    });
   }
-
 }
